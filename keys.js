735041705f7827429a813d3821f12dceed95534d
arjoo <arjubano50@gmaill.com>
2023-06-18 16:31:47 +0530

    First Commit
    First Date: 18-06-23

---
+++ keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mongoUrl:"mongodb+srv://arjoo:JV9msp4ExRND4qXz@cluster0.jzn1qjv.mongodb.net/?retryWrites=true&w=majority",
+    //mongoUrl: "mongodb+srv://FlyingPanda730:UgpngUX6DVAx1Xq0@cluster0.ormku05.mongodb.net/?retryWrites=true&w=majority",
+    Jwt_secret: "faslkfocvneofu"
+
+};
+
+// const  mongoUrl="mongodb+srv://arjoo:JV9msp4ExRND4qXz@cluster0.jzn1qjv.mongodb.net/?retryWrites=true&w=majority"
+// module.exports= mongoUrl;
